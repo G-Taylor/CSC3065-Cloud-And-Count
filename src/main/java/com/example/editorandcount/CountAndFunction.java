0340f8671967f30050a5a6e940d2f1763a26bc28
40234272 <gtaylor18@qub.ac.uk>
2021-12-03 10:28:09 +0000

    sorted issue with regex not working correctly

--- src/main/java/com/example/editorandcount/CountAndFunction.java
+++ src/main/java/com/example/editorandcount/CountAndFunction.java
@@ -9,11 +9,14 @@ public class CountAndFunction {
 
         for (String word : words) {
             // remove all non-alphabetic characters
+//            System.out.println("Word from sentence after split, but before second regex: " + word);
             word = word.replaceAll("[^a-zA-Z]+", "");
 
+//            System.out.println("Word from sentence after second regex: " + word);
             // put all words to lower case and check for ands
             if (word.equalsIgnoreCase("and")){
                count++ ;
+//               System.out.println("Count: " + count);
             }
         }
 
